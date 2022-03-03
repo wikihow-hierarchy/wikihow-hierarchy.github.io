66fe9d8d53dda3721ecab76955ba9a7902885e79
Ziyang Li <liby99@icloud.com>
2022-03-03 21:47:13 +0800

    Initial version of search page

---
+++ js/dataset.js
@@ -0,0 +1,51 @@
+function load_data(callback) {
+  $.getJSON("data/category_list.json", function (category_list) {
+    $.getJSON("data/preprocessed_step_goals.json", function (step_goals) {
+      $.getJSON("data/preprocessed_step_predictions.json", function (step_predictions) {
+        callback(new Dataset(category_list, step_goals, step_predictions));
+      });
+    });
+  });
+}
+
+class Dataset {
+  constructor(category_list, step_goals, step_predictions) {
+    this.category_list = category_list;
+    this.step_goals = step_goals;
+    this.step_predictions = step_predictions;
+  }
+
+  num_tasks() {
+    return Object.keys(this.step_goals).length
+  }
+
+  sample_datapoint() {
+    let id = Math.floor(Math.random() * this.num_tasks());
+    return new Datapoint(Object.keys(this.step_goals)[id], null, null);
+  }
+
+  get_card_data(datapoint, caption_id_prefix) {
+    let task = this.step_goals[datapoint.task_id];
+    return {
+      title: task["task"],
+      elem_id: `${caption_id_prefix}-card`,
+      task_id: datapoint.task_id,
+      tags: task["category"].map((c) => {
+        return {name: this.category_list[c]};
+      }),
+      captions: task["caption"].map((c) => {
+        if (Number.isInteger(c)) {
+          let pred = this.step_predictions[`${c}`];
+          return {
+            caption: pred.name,
+            step_id: c,
+            has_sub_steps: pred.pred.length > 0,
+            elem_id: `${caption_id_prefix}-caption-${c}`,
+          }
+        } else {
+          return {caption: c}
+        }
+      }),
+    }
+  }
+}
--- js/index.js
+++ js/index.js
@@ -22,16 +22,6 @@ $(document).ready(function () {
   });
 });
 
-function load_data(callback) {
-  $.getJSON("data/category_list.json", function (category_list) {
-    $.getJSON("data/preprocessed_step_goals.json", function (step_goals) {
-      $.getJSON("data/preprocessed_step_predictions.json", function (step_predictions) {
-        callback(new Dataset(category_list, step_goals, step_predictions));
-      });
-    });
-  });
-}
-
 class Visualizer {
   constructor() {
     this.card_template = $.templates("#card");
@@ -44,7 +34,7 @@ class Visualizer {
     // Push history
     var new_url = new URL(window.location.href);
     new_url.search = `?task_id=${datapoint.task_id}`;
-    window.history.pushState({ path: new_url.href }, "", new_url.href);
+    window.history.replaceState({ path: new_url.href }, "", new_url.href);
 
     // Get the data
     let data = dataset.get_card_data(datapoint, "middle");
@@ -233,48 +223,6 @@ class Visualizer {
     $("#right").unbind("scroll").scroll(draw);
     $("#middle").unbind("scroll").scroll(draw);
     draw();
-  }
-}
-
-class Dataset {
-  constructor(category_list, step_goals, step_predictions) {
-    this.category_list = category_list;
-    this.step_goals = step_goals;
-    this.step_predictions = step_predictions;
-  }
-
-  num_tasks() {
-    return Object.keys(this.step_goals).length
-  }
-
-  sample_datapoint() {
-    let id = Math.floor(Math.random() * this.num_tasks());
-    return new Datapoint(Object.keys(this.step_goals)[id], null, null);
-  }
-
-  get_card_data(datapoint, caption_id_prefix) {
-    let task = this.step_goals[datapoint.task_id];
-    return {
-      title: task["task"],
-      elem_id: `${caption_id_prefix}-card`,
-      task_id: datapoint.task_id,
-      tags: task["category"].map((c) => {
-        return {name: this.category_list[c]};
-      }),
-      captions: task["caption"].map((c) => {
-        if (Number.isInteger(c)) {
-          let pred = this.step_predictions[`${c}`];
-          return {
-            caption: pred.name,
-            step_id: c,
-            has_sub_steps: pred.pred.length > 0,
-            elem_id: `${caption_id_prefix}-caption-${c}`,
-          }
-        } else {
-          return {caption: c}
-        }
-      }),
-    }
   }
 }
 
