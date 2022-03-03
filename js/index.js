1fda7e0826b80e5925ee27b7de521729bc883045
Ziyang Li <liby99@icloud.com>
2022-03-03 20:23:37 +0800

    Add line in the left

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const LINE_CANVAS_PADDING = 20;
+
 $(document).ready(function () {
   load_data(function (dataset) {
     let visualizer = new Visualizer();
@@ -33,22 +35,28 @@ function load_data(callback) {
 class Visualizer {
   constructor() {
     this.card_template = $.templates("#card");
+    this.history_card_template = $.templates("#history-card");
   }
 
   visualize(datapoint, dataset) {
     $("#curr-task-id").text(datapoint.task_id);
 
+    // Push history
+    var new_url = new URL(window.location.href);
+    new_url.search = `?task_id=${datapoint.task_id}`;
+    window.history.pushState({ path: new_url.href }, '', new_url.href);
+
     // Get the data
-    let data = dataset.get_card_data(datapoint, "left");
+    let data = dataset.get_card_data(datapoint, "middle");
 
     // Render the object onto the page
-    $("#left-inner").html(this.card_template.render(data));
+    $("#middle-inner").html(this.card_template.render(data));
 
     // Setup the callbacks
     data.captions.forEach((caption) => {
       let step_id = caption.step_id;
       if (step_id && caption.has_sub_steps) {
-        $(`#left-caption-${step_id}`).click(() => {
+        $(`#middle-caption-${step_id}`).click(() => {
           this.visualize_step(datapoint, step_id, dataset);
         });
       }
@@ -59,20 +67,99 @@ class Visualizer {
       this.visualize_step(datapoint, datapoint.step_id, dataset);
     } else {
       $("#right-inner").html("");
-      $("#line-canvas").attr("style", "display: none");
+      $("#right-line-canvas").attr("style", "display: none");
+    }
+
+    // If the previous datapoint is present, visualize the parent
+    if (datapoint.parent_datapoint != null) {
+      this.visualize_parent(datapoint.parent_datapoint, dataset);
+    } else {
+      $("#left-inner").html("");
+      $("#left-line-canvas").attr("style", "display: none");
     }
   }
 
+  visualize_parent(parent_datapoint, dataset) {
+    // Open the left canvas
+    $("#left-line-canvas").attr("style", "display: block");
+
+    // Clear the left
+    $("#left-inner").html("");
+
+    // Render the left
+    let data = dataset.get_card_data(parent_datapoint, "left");
+    $("#left-inner").html(this.history_card_template.render(data));
+
+    // Setup on click callback
+    $(`#left-card`).click(() => {
+      this.visualize(parent_datapoint, dataset);
+    });
+
+    // Setup on scroll callback
+    let draw = () => {
+      let svg_elem = $("#left-line-canvas");
+      let svg_x = svg_elem.position().left;
+      let svg_y = svg_elem.position().top;
+
+      // Get d3 svg and clear the canvas
+      let svg = d3.select("#left-line-canvas");
+      svg.selectAll('*').remove();
+
+      // Get middle element position
+      let middle_elem = $("#middle-card .card-left-anchor");
+      let middle_elem_position = middle_elem.position();
+      let middle_x = middle_elem_position.left - svg_x + 6 + LINE_CANVAS_PADDING;
+      let middle_y = middle_elem_position.top - svg_y + 6;
+
+      // Get left element position
+      let left_elem = $("#left-card .card-section-caption-anchor-circle");
+      let left_elem_position = left_elem.position();
+      let left_x = left_elem_position.left - svg_x + 6 + LINE_CANVAS_PADDING;
+      let left_y = left_elem_position.top - svg_y + 6;
+
+      // Draw line
+      svg
+        .append("line")
+        .style("stroke", "rgba(0, 0, 0, 0.5)")
+        .style("stroke-width", 2)
+        .attr("x1", left_x)
+        .attr("y1", left_y)
+        .attr("x2", middle_x)
+        .attr("y2", middle_y);
+
+      // If there is one more level of parent, draw an additional line from left of the screen
+      let base_svg = d3.select("#base-canvas");
+      base_svg.selectAll("*").remove();
+      if (parent_datapoint.parent_datapoint != null) {
+        base_svg.selectAll("*").remove();
+        let edge_x = 0;
+        let left_card = $("#left-card");
+        let left_card_left_x = left_card.position().left + 1;
+        let left_card_middle_y = left_card.position().top + left_card.outerHeight() / 2 + 10;
+        base_svg
+          .append("line")
+          .style("stroke", "rgba(0, 0, 0, 0.2)")
+          .style("stroke-width", 1)
+          .attr("x1", edge_x)
+          .attr("y1", left_card_middle_y)
+          .attr("x2", left_card_left_x)
+          .attr("y2", left_card_middle_y);
+      }
+    };
+    draw();
+  }
+
   visualize_step(parent_datapoint, step_id, dataset) {
-    $("#line-canvas").attr("style", "display: block");
+    // Open the right canvas
+    $("#right-line-canvas").attr("style", "display: block");
 
     // Push history
     var new_url = new URL(window.location.href);
     new_url.search = `?task_id=${parent_datapoint.task_id}&step_id=${step_id}`;
-    window.history.pushState({ path: new_url.href }, '', new_url.href);
+    window.history.replaceState({ path: new_url.href }, '', new_url.href);
 
     // Highlight the step in the left
-    $(`#left-caption-${step_id}`).addClass("active").siblings().removeClass("active");
+    $(`#middle-caption-${step_id}`).addClass("active").siblings().removeClass("active");
 
     // Clear the right
     $("#right-inner").html("");
@@ -101,37 +188,37 @@ class Visualizer {
 
     // Setup a scroll callback
     let draw = () => {
-      let svg_elem = $("#line-canvas");
+      let svg_elem = $("#right-line-canvas");
       let svg_x = svg_elem.position().left;
       let svg_y = svg_elem.position().top;
 
       // Get d3 svg and clear the canvas
-      let svg = d3.select("#line-canvas");
+      let svg = d3.select("#right-line-canvas");
       svg.selectAll('*').remove();
 
-      // Get left element
-      let left_elem = $(`#left-caption-${step_id} .card-section-caption-anchor-circle`);
-      let left_elem_position = left_elem.position();
-      let left_x = left_elem_position.left - svg_x + 16;
-      let left_y = left_elem_position.top - svg_y + 6;
+      // Get middle element
+      let middle_elem = $(`#middle-caption-${step_id} .card-section-caption-anchor-circle`);
+      let middle_elem_position = middle_elem.position();
+      let middle_x = middle_elem_position.left - svg_x + 6 + LINE_CANVAS_PADDING;
+      let middle_y = middle_elem_position.top - svg_y + 6;
       predictions.forEach((p) => {
         let right_elem = $(`#right-${p[0]}-card .card-left-anchor`);
         let right_elem_position = right_elem.position();
-        let right_x = right_elem_position.left - svg_x + 16;
+        let right_x = right_elem_position.left - svg_x + 6 + LINE_CANVAS_PADDING;
         let right_y = right_elem_position.top - svg_y + 6;
 
         // Draw line
         svg
-          .append('line')
+          .append("line")
           .style("stroke", "red")
           .style("stroke-width", 2)
-          .attr("x1", left_x)
-          .attr("y1", left_y)
+          .attr("x1", middle_x)
+          .attr("y1", middle_y)
           .attr("x2", right_x)
           .attr("y2", right_y);
 
         // Draw a text
-        let text_y = right_y > left_y ? right_y + 20 : right_y - 10;
+        let text_y = right_y > middle_y ? right_y + 20 : right_y - 10;
         svg
           .append("text")
           .text(`${p[1]}`.substring(0, 7))
@@ -143,7 +230,7 @@ class Visualizer {
       });
     };
     $("#right").unbind("scroll").scroll(draw);
-    $("#left").unbind("scroll").scroll(draw);
+    $("#middle").unbind("scroll").scroll(draw);
     draw();
   }
 }
@@ -191,9 +278,9 @@ class Dataset {
 }
 
 class Datapoint {
-  constructor(task_id, step_id, previous_datapoint) {
+  constructor(task_id, step_id, parent_datapoint) {
     this.task_id = `${task_id}`;
     this.step_id = step_id;
-    this.previous_datapoint = previous_datapoint;
+    this.parent_datapoint = parent_datapoint;
   }
 }
